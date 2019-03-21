620cb790325659822e8587a7490ce6e3d53cf470
Guido Cioni <guido.cioni@mpimet.mpg.de>
2019-03-21 14:29:48 +0100

    Added script to plot PDFs, cleaned up scripts and folder.

---
+++ plot_pdf.py
@@ -0,0 +1,73 @@
+debug = False 
+if not debug:
+    import matplotlib
+    matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import xarray as xr
+import metpy.calc as mpcalc
+from utils import *
+import sys
+import matplotlib.gridspec as gridspec
+
+dset = xr.open_mfdataset(input_files, concat_dim='ens_member').squeeze()
+dset = dset.metpy.parse_cf()
+time = pd.to_datetime(dset['time'].values)
+# Array needed for the box plot
+pos = np.array((time-time[0]) / pd.Timedelta('1 hour')).astype("int")
+
+myDates= ["2019-03-23 00:00", "2019-03-26 00:00", "2019-03-29 00:00"]
+cities=['Milano','Roma','Palermo']
+var_to_plot='2t'
+
+nrows=np.shape(myDates)[0]
+rows=np.arange(start=0, stop=nrows, step=1)
+gridspec.GridSpec(nrows,1)
+
+for city in cities:
+    lon, lat = get_city_coordinates(city)
+    dset_city =  dset.sel(lon=lon, lat=lat, method='nearest').interpolate_na(dim='time')
+    # Recover units which somehow are deleted by interpolate_na,
+    # no idea why....
+    dset_city['t'].attrs['units'] = 'K'
+    dset_city['t'].metpy.convert_units('degC')
+    dset_city['2t'].attrs['units'] = 'K'
+    dset_city['2t'].metpy.convert_units('degC')
+
+    fig = plt.figure(1, figsize=(10, 6))
+
+    sns.set(style="white", palette="muted", color_codes=True)
+    sns.set_context(rc={"lines.linewidth": 0.5})
+    plt.rc_context({'axes.edgecolor':'grey', 'xtick.color':'grey', 'ytick.color':'grey'})
+    plt.suptitle("GEFS PDF forecast for "+city+" | Run "+(time[0]-np.timedelta64(6,'h')).strftime('%Y%m%d %H UTC'))
+
+    for row in rows:
+        ds = dset_city[var_to_plot].sel(time=myDates[row], method='nearest')
+        if row == rows[0]:
+            ax1=plt.subplot2grid((nrows,1), (row,0))
+        else: 
+            plt.subplot2grid((nrows,1), (row,0), sharex=ax1)
+        
+        ax2 = sns.distplot(ds, hist=False, kde_kws={"shade":True})
+        
+        ax3 = ax2.twinx()
+        ax3.get_xaxis().set_visible(False)
+        sns.boxplot(x=ds, ax=ax3)
+        sns.despine()
+        ax3.set(ylim=(-4, 6))
+        at = AnchoredText(myDates[row], prop=dict(size=10), frameon=True,loc=2)
+        at.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+        plt.gca().add_artist(at)
+        ax2.yaxis.set_major_locator(plt.MaxNLocator(3))
+        if row != rows[-1]:
+            ax2.get_xaxis().set_visible(False)
+        else: 
+            ax2.get_xaxis().set_visible(True)
+            ax2.set_xlabel('Temperatura a 2 metri [C]')
+
+    if debug:
+        plt.show(block=True)
+    else:
+        plt.savefig('gefs_pdf_%s.png' % city, dpi=100, bbox_inches='tight') 
--- plot_spaghetti.py
+++ plot_spaghetti.py
@@ -1,71 +1,71 @@
-# coding: utf-8
-debug = True 
+debug = False 
 if not debug:
     import matplotlib
     matplotlib.use('Agg')
 
-import xarray as xr
 import matplotlib.pyplot as plt
-import numpy as np # Import the Numpy package
-from datetime import datetime
-from mpl_toolkits.axes_grid.anchored_artists import AnchoredText
-import matplotlib.gridspec as gridspec
+import numpy as np
 from glob import glob
+import seaborn as sns
 import pandas as pd
 import matplotlib.dates as mdates
-import seaborn as sns
-from geopy.geocoders import Nominatim
+from matplotlib.dates import DateFormatter
+import xarray as xr
+import metpy.calc as mpcalc
+from utils import *
+import sys
 
-diri='/scratch/local1/m300382/gens/grib/'
-diri_images='/scratch/local1/m300382/gens/'
+folder_images='/home/mpim/m300382/gens/'
 
-fileslist=sorted(glob(diri+"*.nc"))
-datasets = [xr.open_dataset(files) for files in fileslist]
-# Merging should take care automatically of solving every conflict in the dimensions
-merged = xr.concat(datasets, 'ens_member').squeeze()
+# Get the projection as system argument from the call so that we can 
+# span multiple instances of this script outside
+if not sys.argv[1:]:
+    print('City not defined, falling back to default (Hamburg)')
+    cities = ['Hamburg']
+else:    
+    cities=sys.argv[1:]
 
-merged['t']= merged['t'] - 273.15
-n_pert=merged['ens_member'].values
-
-time=pd.to_datetime(merged['time'].values)
-
-t_850hpa_point={}
-tot_prec_point={}
-snow_point={}
-cities = ["Milano","Roma","Palermo"]
-geolocator = Nominatim(user_agent='gefs')
-for city in cities:
-    loc = geolocator.geocode(city)
-    t_850hpa_point[city] = merged['t'].sel(lon=loc.longitude, lat=loc.latitude, method='nearest').interpolate_na(dim='time')
-    tot_prec_point[city] = merged['tp'].sel(lon=loc.longitude, lat=loc.latitude, method='nearest').interpolate_na(dim='time')
-    snow_point[city]     = merged['csnow'].sel(lon=loc.longitude, lat=loc.latitude, method='nearest').interpolate_na(dim='time')
+dset = xr.open_mfdataset(input_files, concat_dim='ens_member').squeeze()
+dset = dset.metpy.parse_cf()
+time = pd.to_datetime(dset['time'].values)
+# Array needed for the box plot
+pos = np.array((time-time[0]) / pd.Timedelta('1 hour')).astype("int")
 
 sns.set(style="white")
-var_to_plot=t_850hpa_point
-var2_to_plot=tot_prec_point
 
-for city_to_plot in cities:
+for city in cities:
+    print('Producing meteogram for %s' % city)
+    lon, lat = get_city_coordinates(city)
+    dset_city =  dset.sel(lon=lon, lat=lat, method='nearest').interpolate_na(dim='time')
+    # Recover units which somehow are deleted by interpolate_na,
+    # no idea why....
+    dset_city['t'].attrs['units'] = 'K'
+    dset_city['t'].metpy.convert_units('degC')
+
     fig = plt.figure(1, figsize=(9,6))
     ax = plt.gca()
-    plt.plot(time, var_to_plot[city_to_plot].T,lw=1)
-    plt.plot(time, var_to_plot[city_to_plot].T[:,np.where(n_pert==0)[0]],'-',lw=2.5,color='black')
+    plt.plot(time, dset_city['t'].T, lw=1)
+    plt.plot(time, dset_city['t'].sel(ens_member=0),'-', lw=2.5, color='black')
     plt.ylabel("850 hPa temperature [C]")
-    plt.ylim(-25, 20)
+    plt.ylim(-10, 15)
     ax.minorticks_on()
-    ax.grid(which='major', linestyle='-', linewidth='0.5', color='black')
-    ax.grid(which='minor', linestyle='-', linewidth='0.3', color='black')
+    ax.grid(which='major', linestyle='-', linewidth='0.3', color='black')
+    ax.grid(which='minor', linestyle='-', linewidth='0.1', color='black')
+    ax.set_xlim(time[0], time[-1])
+    ax.fill_between(time, dset_city['t'].min(dim='ens_member'), dset_city['t'].max(dim='ens_member'),
+                     facecolor='gray', alpha=0.3)
+
+    # ax2 = ax.twinx()
+    # ax2.plot(time, dset_city['tp'].T,'-', lw=1)
+    # ax2.set_ylabel("Precipitation [mm]")
+    # ax2.set_ylim(0, 50)
     
-    ax2 = ax.twinx()
-    ax2.plot(time,var2_to_plot[city_to_plot].T,'--',lw=1.5)
-    ax2.set_ylabel("Precipitation [mm]")
-    ax2.set_ylim(0, 50)
-    
-    plt.title("GEFS forecast for "+city_to_plot+" | Run "+time[0].strftime("%Y%m%d %H"))
+    plt.title("GEFS forecast for "+city+" | Run "+(time[0]-np.timedelta64(6,'h')).strftime('%Y%m%d %H UTC'))
     fig.autofmt_xdate()
     
     if debug:
         plt.show(block=True)
     else:
-        plt.savefig(diri_images+"spaghetti_"+city_to_plot, dpi=100, bbox_inches='tight') 
+        plt.savefig(folder_images+"spaghetti_"+city, dpi=100, bbox_inches='tight') 
 
     plt.clf()
